3859cf78cf65de1fb3a1e74512fd5cd963c60392
Victor Emanuel <victor.emanuel@esig.com.br>
2021-05-16 17:15:57 -0300

    Corrige bug na versao do Spring Cloud

--- src/main/java/zup/garagem/client/FIPEClient.java
+++ src/main/java/zup/garagem/client/FIPEClient.java
@@ -3,20 +3,12 @@ package zup.garagem.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import zup.garagem.dto.FIPEItemDTO;
-import zup.garagem.dto.ModeloCarroDTO;
 import zup.garagem.dto.VeiculoDTO;
 
 import java.util.List;
 
-@FeignClient(url = "https://parallelum.com.br/fipe/api/v1/carros/")
+@FeignClient(name = "fipeClient", url = "https://parallelum.com.br/fipe/api/v1/carros/")
 public interface FIPEClient {
-    @GetMapping("marcas/")
-    public List<FIPEItemDTO> getMarcas();
-
-    @GetMapping("modelos/{modeloId}")
-    public List<ModeloCarroDTO> getModelos(@PathVariable Integer modeloId);
-
     @GetMapping("marcas/{marcaId}/modelos/{modeloId}/anos/{anoCodigo}")
-    public List<VeiculoDTO> getVeiculo(@PathVariable Integer marcaId, @PathVariable Integer modeloId, @PathVariable String anoCodigo);
+    List<VeiculoDTO> getVeiculo(@PathVariable Integer marcaId, @PathVariable Integer modeloId, @PathVariable String anoCodigo);
 }
--- src/main/java/zup/garagem/controller/VeiculoController.java
+++ src/main/java/zup/garagem/controller/VeiculoController.java
@@ -1,8 +1,5 @@
 package zup.garagem.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -13,7 +10,6 @@ import zup.garagem.dto.ErroValidacaoDTO;
 import zup.garagem.dto.VeiculoDTO;
 import zup.garagem.repository.VeiculoRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,31 +20,30 @@ public class VeiculoController {
     private final VeiculoRepository veiculoRepository;
     private final FIPEClient fipeClient;
 
-    public VeiculoController(VeiculoRepository veiculoRepository, FIPEClient fipeClient){
+    public VeiculoController(VeiculoRepository veiculoRepository, FIPEClient fipeClient) {
         this.veiculoRepository = veiculoRepository;
         this.fipeClient = fipeClient;
     }
 
     @GetMapping
-    public ResponseEntity<List<VeiculoDTO>> listar(){
+    public ResponseEntity<List<VeiculoDTO>> listar() {
         var veiculosDTO = veiculoRepository
                 .findAll()
                 .stream()
-                .map(v->v.toDTO())
+                .map(v -> v.toDTO())
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(veiculosDTO);
     }
 
     @PostMapping
-    public ResponseEntity<?> cadastrar(@Validated @RequestBody VeiculoDTO v, BindingResult result){
+    public ResponseEntity<?> cadastrar(@Validated @RequestBody VeiculoDTO v, BindingResult result) {
         if (result.hasErrors()) {
             ErroValidacaoDTO erro = new ErroValidacaoDTO(result, "Erro ao cadastrar veículo");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
         }
 
         var novoVeiculo = v.toVeiculo();
-//        var marca =
         novoVeiculo = veiculoRepository.save(novoVeiculo);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoVeiculo.toDTO());
     }
